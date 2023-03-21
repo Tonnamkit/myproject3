fd4f72935aff38591c2a96480b8f9c2c0fa8f088
Tonnamkit <kittipat.bual@mail.kmutt.ac.th>
2023-03-21 14:16:54 +0700

    version1.1

--- myproject3.java
+++ myproject3.java
@@ -2,6 +2,6 @@ package myproject3;
 
 public class myproject3 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world~~~~~~~~");
     }
 }
